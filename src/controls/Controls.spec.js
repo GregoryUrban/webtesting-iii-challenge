f51fcfa99b63fdf30bbc3b67c1a167b47e9c5e65
GregoryUrban <turknben@gmail.com>
2019-06-25 16:06:15 -0400

    Control component finished

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,53 +1,65 @@
 // Test away!
 // ### Controls Component
 
-// - provide buttons to toggle the `closed` and `locked` states.
-// - buttons' text changes to reflect the state the door will be in if clicked
-// - the closed toggle button is disabled if the gate is locked
-// - the locked toggle button is disabled if the gate is open
+// - provide buttons to toggle the `closed` and `locked` states. - line 20
+// - buttons' text changes to reflect the state the door will be in if clicked - lines 29, 40
+// - the closed toggle button is disabled if the gate is locked. Line 50
+// - the locked toggle button is disabled if the gate is open. Line 58
 
 
 import React from 'react';
 import renderer from 'react-test-renderer'
-import { render, fireEvent } from '@testing-library/react'; // after installing this, import
+import { render, fireEvent }  from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each'; // keeps the test from adding a new app everytime - cleans up the tests
 
 import Controls from './Controls';
 
 
 describe('<Controls />', () => {
-    xit('matches snapshot', () => { // identifies if anything changes after component is finished
-        const tree = renderer.create(<Controls />)
+
+    it("should provide buttons to toggle the closed and locked states", async () => {
+        const buttons = render(<Controls locked={false} closed={false}/>);
+        const closebtn = buttons.getByTestId('toggle-closed');
+        const lockbtn = buttons.getByTestId('toggle-locked');
+        expect(closebtn).toBeDefined();
+        expect(lockbtn).toBeDefined();
+    })
+
+    it('Unlocked buttons text changes to reflect the state the door will be in if clicked', async () => {
+        // const buttons;  const closebtn; const lockbtn;
+        const buttons = render(<Controls locked={false} closed={false}/>);
+        const closebtn = buttons.getByTestId('toggle-closed');
+        const lockbtn = buttons.getByTestId('toggle-locked');
+        expect(closebtn.textContent).toBe("Close Gate");
+        expect(lockbtn.textContent).toBe("Lock Gate");
+        await fireEvent.click(closebtn);
+        await fireEvent.click(lockbtn);
+    })
+
+    it('Locked buttons text changes to reflect the state the door will be in if clicked', async () => {
+        const buttons = render(<Controls locked={true} closed={true}/>);
+        const closebtn = buttons.getByTestId('toggle-closed');
+        const lockbtn = buttons.getByTestId('toggle-locked');
+        expect(closebtn.textContent).toBe("Open Gate");
+        expect(lockbtn.textContent).toBe("Unlock Gate");
+        await fireEvent.click(closebtn);
+        await fireEvent.click(lockbtn);
+    })
+
+    it("the closed toggle button is disabled if the gate is locked", async () => {
+        const buttons = render(<Controls locked={true} closed={true}/>);
+        const button = buttons.getByTestId('toggle-closed');
+        await fireEvent.click(button);
+        expect(button.disabled).toBe(true);
+    })
+
+    it("the locked toggle button is disabled if the gate is open", async () => {
+        const buttons = render(<Controls locked={false} closed={false}/>);
+        const button = buttons.getByTestId('toggle-locked');
+        await fireEvent.click(button);
+        expect(button.disabled).toBe(true);
+    })
     
-        expect(tree.toJSON()).toMatchSnapshot()
-      })
-
-     xit('matches snapshot with message', () => { // identifies if anything changes after component is finished
-        const tree = renderer.create(<Controls message={'hello'}/>)
-    
-        expect(tree.toJSON()).toMatchSnapshot()
-      })
-
-     xit('should display message', () => { 
-        const message = 'hello'
-
-        const { getByText } = render(<Controls message={message}/>)
-    
-        getByText(message)
-      })
-
-      it('should call closed on button click', () => {
-          const locked = jest.fn(); //creates mock function
-
-          const { getByText } = render(<Controls locked={locked}/>)
-
-          const button = getByText(/speak/i)
-
-          fireEvent.click(button);
-        //   fireEvent.click(button);
-
-          expect(speak).toHaveBeenCalled();
-          expect(speak).toHaveBeenCalledTimes(1);
-      })
 
 })
+
